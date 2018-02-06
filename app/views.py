93cf18050c071a2ead0999d1f50a50b16309c232
Danilo-Zekovic <danilozeka93@gmail.com>
2018-02-06 10:46:13 +0100

    Sceleton done

--- app/views.py
+++ app/views.py
@@ -18,19 +18,21 @@ def generate(template_name, output_name, render_vars):
     rendered = template.render(render_vars)
     print(rendered)
     #i pisemo u fajl
-    file_name = os.path.join(root, "app/templates/output", output_name)
+    file_name = os.path.join(root, "templates/output", output_name)
     print(file_name)
     with open(file_name, "w+") as f:
         f.write(rendered)
 
-
+# TODO: napraviti da u request-u se prosledi koji tmplate (quiz ili survey), verovatno neki naziv path itd
 def gen(request):
     debug=False
-    model = execute(os.path.join(root, "app/generator"), 'quiz.tx', 'example.quiz', debug, debug)
-    modelsurvey = execute(os.path.join(root, "app/generator"), 'quiz.tx', 'example1.survey', debug, debug)
+    #TODO: smanjiti da bude samo jedan model i jedan generator koji ce dobijati parametre na osnovu kojih ce raditi mesto hardcodovanja
+    model = execute(os.path.join(root, "generator"), 'quiz.tx', 'example.quiz', debug, debug)
+    modelsurvey = execute(os.path.join(root, "generator"), 'quiz.tx', 'example1.survey', debug, debug)
 
     generate("test_template.html", "quiz.html", {"page": model})
     generate("survey_template.html", "survey.html", {"page": modelsurvey})
+    # TODO: promeniti tako da ne dira base vec samo da modifikuje likove za ponudjene quiz i survey
     generate("home_template.html", "index.html", {"page": model})
 
     return render(request, 'quizgen/home.html', {'data':'Success!', 'model':model, 'modelsurvey':modelsurvey})
