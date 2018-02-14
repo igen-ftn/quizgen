cc1ad51c58b294911c2267b079833a9cf7408189
unknown <ana94ns@hotmail.com>
2018-02-14 16:16:46 +0100

    finished quiz input #7

--- app/static/app/grammar.js
+++ app/static/app/grammar.js
@@ -4,23 +4,29 @@ var questionNum = 1;
 function previewQuiz() {
 
         $('#previewDiv').empty();
+        questionNum = $("#questionDiv").children().length;
 
         var grammar ="<br/><p>Start test</p>"
          + "<p>" + "&nbsp;".repeat(4) + "\"" + $('#title').val() + "\"" +"</p>"
-         + "<p>" + "&nbsp;".repeat(4) + "Start quiz" +"</p>"
-         + "<p>" + "&nbsp;".repeat(8) + $('#question1').val() +"</p>";
-         for (i = 1; i < answerNum; i++) {
-            var answer = $('#q' + questionNum + 'answer' + i).val();
-            grammar += "<p>" + "&nbsp;".repeat(16) + answer;
-            if (document.getElementById("q" + questionNum + "ans" + i).checked){
-                grammar += " +";
+         + "<p>" + "&nbsp;".repeat(4) + "Start quiz" +"</p>";
+
+         for (i = 1; i <= questionNum; i++) {
+             var answerNum = $("#answersDiv" + i).children().length;
+             grammar += "<p>" + "&nbsp;".repeat(8) + $('#question' + i).val() +"</p>";
+             for (j = 1; j <= answerNum; j++) {
+                var answer = $('#q' + i + 'answer' + j).val();
+                grammar += "<p>" + "&nbsp;".repeat(16) + answer;
+                if (document.getElementById("q" + i + "ans" + j).checked){
+                    grammar += " +";
+                }
+                else{
+                    grammar += " -";
+                }
+                grammar += "</p>";
             }
-            else{
-                grammar += " -";
-            }
-            grammar += "</p>";
-        }
-         grammar += "<p>" + "&nbsp;".repeat(4) + "End quiz" +"</p>"
+         }
+         grammar += "<p>" + "&nbsp;".repeat(8) + "button ok \"Submit\"" +"</p>"
+         + "<p>" + "&nbsp;".repeat(4) + "End quiz" +"</p>"
          + "<p>End test</p>";
 
         $('#previewDiv').append(grammar);
@@ -31,6 +37,8 @@ function previewQuiz() {
 }
 
 function addAnswer(questionNum) {
+
+        var answerNum = $("#answersDiv" + questionNum).children().length + 1;
 
         var newAnswer = "<div class=\"form-group\">" +
                     "<label class=\"col-sm-2 control-label\" style=\"text-align:left; width:100px;\">Answer:</label>" +
@@ -45,16 +53,14 @@ function addAnswer(questionNum) {
                 "</div>"
 
         $('#answersDiv' + questionNum).append(newAnswer);
-        answerNum++;
 
 }
 
 function addQuestion() {
 
-        questionNum++;
-        answerNum = 3;
+        questionNum = $("#questionDiv").children().length + 1;
 
-        var newQuestion = "<div class=\"form-group\">" +
+        var newQuestion = "<div><div class=\"form-group\">" +
                     "<label class=\"col-sm-2 control-label\" style=\"text-align:left; width:100px;\">Question:</label>" +
                     "<div class=\"col-sm-8\">" +
                         "<textarea  class=\"form-control\" id=\"question" + questionNum + "\" rows=\"5\" style=\"width:88%; resize:none;\">Question &lt;order&gt; &lt;\" + text + \"&gt; &lt;'one answer'/'multiple answers'&gt;</textarea>" +
@@ -69,10 +75,10 @@ function addQuestion() {
                     "<div class=\"form-group\">" +
                         "<label class=\"col-sm-2 control-label\" style=\"text-align:left; width:100px;\">Answer:</label>" +
                         "<div class=\"col-sm-8 form-inline\">" +
-                          "<input type=\"text\" class=\"form-control\" id=\"q" + questionNum + "answer" + answerNum + "\" style=\"width:70%\" value=\"Answer <&quot + tag + &quot> <&quot + text + &quot>\">" +
+                          "<input type=\"text\" class=\"form-control\" id=\"q" + questionNum + "answer1\" style=\"width:70%\" value=\"Answer <&quot + tag + &quot> <&quot + text + &quot>\">" +
                             "<div class=\"checkbox checkbox-circle\" style=\"margin-left:43px;\">" +
-                                "<input id=\"q" + questionNum + "ans" + answerNum + "\" type=\"checkbox\">" +
-                                "<label for=\"q" + questionNum + "ans" + answerNum + "\">" +
+                                "<input id=\"q" + questionNum + "ans1\" type=\"checkbox\">" +
+                                "<label for=\"q" + questionNum + "ans1\">" +
                                 "</label>" +
                             "</div>" +
                         "</div>" +
@@ -83,17 +89,16 @@ function addQuestion() {
                     "<div class=\"form-group\">" +
                         "<label class=\"col-sm-2 control-label\" style=\"text-align:left; width:100px;\">Answer:</label>" +
                         "<div class=\"col-sm-8 form-inline\">" +
-                            "<input type=\"text\"  class=\"form-control\" id=\"q1answer" + questionNum + "\" style=\"width:70%\" value=\"Answer <&quot + tag + &quot> <&quot + text + &quot>\">" +
+                            "<input type=\"text\"  class=\"form-control\" id=\"q" + questionNum + "answer2\" style=\"width:70%\" value=\"Answer <&quot + tag + &quot> <&quot + text + &quot>\">" +
                             "<div class=\"checkbox checkbox-circle\" style=\"margin-left:43px;\">" +
-                                "<input id=\"q" + questionNum + "ans" + answerNum + "\" type=\"checkbox\">" +
-                                "<label for=\"q" + questionNum + "ans" + answerNum + "\">" +
+                                "<input id=\"q" + questionNum + "ans2\" type=\"checkbox\">" +
+                                "<label for=\"q" + questionNum + "ans2\">" +
                                 "</label>" +
                             "</div>" +
                         "</div>" +
                     "</div>" +
-                "</div><br/>";
+                "</div><br/></div>";
 
         $('#questionDiv').append(newQuestion);
-        answerNum++;
 
 }
