e68ab43e44bd99bc8bbb1672f1b6fea69821e2ac
kimnovak <kimnovak187@gmail.com>
2018-02-16 11:59:52 +0100

    added csrf token to ajax request header #13

--- app/static/app/grammar.js
+++ app/static/app/grammar.js
@@ -97,12 +97,39 @@ function addQuestion() {
 
 }
 
+function getCookie(name) {
+    var cookieValue = null;
+    if(document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if(cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
 function submitQuiz() {
 
         previewQuiz();
         var node = document.getElementById('previewDiv')
         console.log(node.textContent);
         var url = '/new_quiz/';
+        var csrftoken = getCookie('csrftoken')
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if(!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+        });
         $.ajax({
             type: "POST",
             url: url,
